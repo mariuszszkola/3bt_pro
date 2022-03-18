7936665cc43da47e7a053ff482cf12c5a8cb8346
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2022-03-18 08:23:05 +0100

    n1
    n1

---
+++ form1.php
@@ -0,0 +1,25 @@
+<?php
+require '11o.php';
+
+
+$samochod1 = new samochod($_POST['marka'], $_POST['model'],$_POST['rok'], $_POST['cena'], $_POST['pojemnosc'],$_POST['moc'], $_POST['vin'],$_POST['przebieg'] , $_POST['ilosc_paliwa'], $_POST['spalanie']);
+$samochod1->zapisz($samochod1->marka,
+                    $samochod1->model,
+                    $samochod1->rok,
+                    $samochod1->cena,
+                    $samochod1->pojemnosc,
+                    $samochod1->moc,
+                    $samochod1->VIN,
+                    $samochod1->przebieg,
+                    $samochod1->ilosc_paliwa,
+                    $samochod1->spalanie
+                );
+
+
+
+echo "XXXXXXXXXXXXXXXXXXXXXXXXXXX<BR><BR>";
+
+$samochod1->odczyt_vin("aaaaaaaaaaaaaaaaa");
+
+
+?>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dane samochodow</title>
+</head>
+<body>
+
+
+<header>
+    <div class="gora">
+<p>ddd</p>
+
+    </div>
+</header>
+
+<div class="zawartosc">
+    <div class="lewa">
+    <a href="form1.html"><div class="menu">
+        <ul>
+        <li>Dodaj samochód</li>
+        </ul>
+        </div> <!--end menu --></a>
+
+
+
+    </div>
+    <div class="srodek"><p>ddd</p> </div>
+    
+    <div class="prawa"><p>ddd</p></div>
+    
+
+</div>
+
+    
+</body>
+</html>
