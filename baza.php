4f78bde302f3e653fb8663efb4eb11606f133201
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2022-04-25 12:59:37 +0200

    Update baza.php

--- baza.php
+++ baza.php
@@ -4,7 +4,11 @@ $host="localhost";
 $login_db="root";
 $password_db="";
 $base="motoryzacja";
-$conn = new mysqli($host,$login_db,$password_db,$base);
-           
+//$conn = new mysqli($host,$login_db,$password_db,$base);
+  
+$d = "mysql:host=localhost;db_name=motoryzacja";
+
+$conn = new PDO($d, $login_db, $password_db);
 
 ?>
+
